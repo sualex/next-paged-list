0b14cb027b34eee35a6a12d9546770f13cf50dd2
sualexx <a153048@gmail.com>
2024-01-26 15:48:12 +0300

    clean up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import { css } from "@emotion/react";
 import { Stack } from "@mui/material";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
