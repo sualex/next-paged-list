44aa9dda120aab5425d900e3ff62f880e2edccd4
sualexx <a153048@gmail.com>
2024-01-26 14:20:49 +0300

    fix react-query setup by guidelines

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,51 @@
 import { AppCacheProvider } from "@mui/material-nextjs/v14-pagesRouter";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useState } from "react";
 
 import theme from "@/app/theme";
 
-const queryClient = new QueryClient();
-
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
+
+  // Instead do this, which ensures each request has its own cache:
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+
   return (
-    <AppCacheProvider {...props}>
+    <>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
+      <AppCacheProvider {...props}>
         <QueryClientProvider client={queryClient}>
-          <ReactQueryDevtools initialIsOpen={false} />
-          <Component {...pageProps} />
+          <HydrationBoundary state={pageProps.dehydratedState}>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <ReactQueryDevtools initialIsOpen={false} />
+              <Component {...pageProps} />
+            </ThemeProvider>
+          </HydrationBoundary>
         </QueryClientProvider>
-      </ThemeProvider>
-    </AppCacheProvider>
+      </AppCacheProvider>
+    </>
   );
 }
